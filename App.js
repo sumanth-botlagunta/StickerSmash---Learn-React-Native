cd3f8d3d7d394608382c4fb2b8c055c1da54f50d
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2023-03-22 21:26:07 +0530

    added gestures

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import ImageViewer from './components/ImageViewer'
 import Button from './components/Button';
@@ -53,30 +54,32 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
-        {pickedEmoji !== null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji} /> : null}
-      </View>
-      {showAppOptions ? (
-        <View style={styles.optionsContainer}>
-          <View style={styles.optionsRow}>
-            <IconButton icon="refresh" label="reset" onPress={onReset} />
-            <CircleButton onPress={onAddSticker} />
-            <IconButton icon='save-alt' label='save' onPress={onSaveImageAsync} />
+    <GestureHandlerRootView style={styles.container}>
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
+          {pickedEmoji !== null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji} /> : null}
+        </View>
+        {showAppOptions ? (
+          <View style={styles.optionsContainer}>
+            <View style={styles.optionsRow}>
+              <IconButton icon="refresh" label="reset" onPress={onReset} />
+              <CircleButton onPress={onAddSticker} />
+              <IconButton icon='save-alt' label='save' onPress={onSaveImageAsync} />
+            </View>
           </View>
-        </View>
-      ) : (
-        <View style={styles.footerContainer}>
-          <Button theme='primary' label="Choose a Photo" onPress={pickImageasync} />
-          <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
-        </View>
-      )}
-      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
-      </EmojiPicker>
-      <StatusBar style="auto" />
-    </View>
+        ) : (
+          <View style={styles.footerContainer}>
+            <Button theme='primary' label="Choose a Photo" onPress={pickImageasync} />
+            <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+          </View>
+        )}
+        <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+          <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+        </EmojiPicker>
+        <StatusBar style="auto" />
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -1,13 +1,58 @@
 import { View, Image } from 'react-native';
+import Animated, { useAnimatedStyle, useSharedValue, useAnimatedGestureHandler, withSpring } from 'react-native-reanimated';
+import { TapGestureHandler, PanGestureHandler } from 'react-native-gesture-handler';
+
+
+const AnimatedImage = Animated.createAnimatedComponent(Image);
+const AnimatedView = Animated.createAnimatedComponent(View);
 
 export default function EmojiSticker({ imageSize, stickerSource }) {
+    const scaleImage = useSharedValue(imageSize)
+    const onDoubleTap = useAnimatedGestureHandler({
+        onActive: () => {
+            if (scaleImage.value) {
+                scaleImage.value = scaleImage.value * 2
+            }
+        }
+    })
+    const imageStyle = useAnimatedStyle(() => {
+        return {
+            width: withSpring(scaleImage.value),
+            height: withSpring(scaleImage.value)
+        }
+    })
+    const translateX = useSharedValue(0)
+    const translateY = useSharedValue(0)
+    const onDrag = useAnimatedGestureHandler({
+        onStart: (_, ctx) => {
+            ctx.startX = translateX.value
+            ctx.startY = translateY.value
+        },
+        onActive: (event, ctx) => {
+            translateX.value = ctx.startX + event.translationX
+            translateY.value = ctx.startY + event.translationY
+        }
+    })
+
+    const containerStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                { translateX: translateX.value },
+                { translateY: translateY.value }
+            ]
+        }
+    })
     return (
-        <View style={{ top: -350 }}>
-            <Image
-                source={stickerSource}
-                resizeMode="contain"
-                style={{ width: imageSize, height: imageSize }}
-            />
-        </View>
+        <PanGestureHandler onGestureEvent={onDrag}>
+            <AnimatedView style={[containerStyle, { top: -350 }]}>
+                <TapGestureHandler onGestureEvent={onDoubleTap} numberOfTaps={2}>
+                    <AnimatedImage
+                        source={stickerSource}
+                        resizeMode="contain"
+                        style={[imageStyle, { width: imageSize, height: imageSize }]}
+                    />
+                </TapGestureHandler>
+            </AnimatedView>
+        </PanGestureHandler>
     );
 }
