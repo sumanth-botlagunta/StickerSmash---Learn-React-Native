bc5860c17054d99ff38ce17c8f39eb3371800e32
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2023-03-22 18:51:43 +0530

    image picker + choose photo

--- App.js
+++ App.js
@@ -6,14 +6,33 @@ import Button from './components/Button';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
+import { useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
+
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImageasync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("You didn't select an image")
+    }
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+        <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme='primary' label="Choose a Photo" />
+        <Button theme='primary' label="Choose a Photo" onPress={pickImageasync} />
         <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
